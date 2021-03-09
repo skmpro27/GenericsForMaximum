2c7626096425df2254c46c675af37f2bf9dc94bf
unknown <kumar.skm03@gmail.com>
2021-03-09 20:14:55 +0530

    refactor code to compare generic type in GenericsForMaximum.java and change method accordingly in GenericsForMaximumTest.java

--- src/main/java/com/generics/GenericsForMaximum.java
+++ src/main/java/com/generics/GenericsForMaximum.java
@@ -2,30 +2,12 @@ package com.generics;
 
 public class GenericsForMaximum {
 
-    public Integer compareIntegerForMax(Integer num1, Integer num2, Integer num3) {
-        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        else if (num2.compareTo(num3) > 0)
-            return num2;
+    public <E extends Comparable<E>> E compareGenericForMax(E value1, E value2, E value3) {
+        if (value1.compareTo(value2) > 0 && value1.compareTo(value3) > 0)
+            return value1;
+        else if (value2.compareTo(value3) > 0)
+            return value2;
         else
-            return num3;
-    }
-
-    public Double compareDoubleForMax(Double num1, Double num2, Double num3) {
-        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        else if (num2.compareTo(num3) > 0)
-            return num2;
-        else
-            return num3;
-    }
-
-    public String compareStringForMax(String str1, String str2, String str3) {
-        if (str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0)
-            return str1;
-        else if (str2.compareTo(str3) > 0)
-            return str2;
-        else
-            return str3;
+            return value3;
     }
 }
--- src/test/java/com/generics/GenericsForMaximumTest.java
+++ src/test/java/com/generics/GenericsForMaximumTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class GenericsForMaximumTest {
 
-    GenericsForMaximum operation;
+    public GenericsForMaximum operation;
 
     @Before
     public void setUp() throws Exception {
@@ -15,55 +15,55 @@ public class GenericsForMaximumTest {
 
     @Test
     public void givenMaxNumberFirstPositionReturnSameInteger() {
-        int result = operation.compareIntegerForMax(7, 2, 5);
+        int result = operation.compareGenericForMax(7, 2, 5);
         Assert.assertEquals(7, result);
     }
 
     @Test
     public void givenMaxNumberSecondPositionReturnSameInteger() {
-        int result = operation.compareIntegerForMax(7, 8, 5);
+        int result = operation.compareGenericForMax(7, 8, 5);
         Assert.assertEquals(8, result);
     }
 
     @Test
     public void givenMaxNumberThirdPositionReturnSameInteger() {
-        int result = operation.compareIntegerForMax(7, 2, 10);
+        int result = operation.compareGenericForMax(7, 2, 10);
         Assert.assertEquals(10, result);
     }
 
     @Test
     public void givenMaxNumberFirstPositionReturnSameDouble() {
-        double result = operation.compareDoubleForMax(7.0, 2.1, 5.6);
+        double result = operation.compareGenericForMax(7.0, 2.1, 5.6);
         Assert.assertEquals(7.0, result, 1e-15);
     }
 
     @Test
     public void givenMaxNumberSecondPositionReturnSameDouble() {
-        double result = operation.compareDoubleForMax(7.6, 8.9, 5.4);
+        double result = operation.compareGenericForMax(7.6, 8.9, 5.4);
         Assert.assertEquals(8.9, result, 1e-15);
     }
 
     @Test
     public void givenMaxNumberThirdPositionReturnSameDouble() {
-        double result = operation.compareDoubleForMax(7.5, 2.8, 10.6);
+        double result = operation.compareGenericForMax(7.5, 2.8, 10.6);
         Assert.assertEquals(10.6, result, 1e-15);
     }
 
     @Test
     public void givenMaxNumberFirstPositionReturnSameString() {
-        String result = operation.compareStringForMax("Cat", "Apple", "Ball");
+        String result = operation.compareGenericForMax("Cat", "Apple", "Ball");
         Assert.assertEquals("Cat", result);
     }
 
     @Test
     public void givenMaxNumberSecondPositionReturnSameString() {
-        String result = operation.compareStringForMax("Dog", "dog", "Cat");
+        String result = operation.compareGenericForMax("Dog", "dog", "Cat");
         Assert.assertEquals("dog", result);
     }
 
     @Test
     public void givenMaxNumberThirdPositionReturnSameString() {
-        String result = operation.compareStringForMax("Hen", "Dog", "Van");
+        String result = operation.compareGenericForMax("Hen", "Dog", "Van");
         Assert.assertEquals("Van", result);
     }
 }
